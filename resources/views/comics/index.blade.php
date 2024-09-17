ff79f2dac3fe55f99b24d674ade4f87d607334d8
Samgrass <samuelegrassetti01@gmail.com>
2024-09-17 18:15:00 +0200

    create index with table of comics from db

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container my-5">
+    <h1>I miei Fumetti</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#id</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Immagine</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Rilascio</th>
+                <th scope="col">Strumenti</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+            <tr>
+                <th>{{ $comic->id }}</th>
+                <td>{{ $comic->title}}</td>
+                <td><img width="20%" src="{{ $comic->thumb}}" alt=""></td>
+                <td>{{ $comic->price}}</td>
+                <td>{{ $comic->type}}</td>
+                <td>{{ $comic->sale_date}}</td>
+                <td>
+                    <div class="d-flex gap-1">
+                        <a href="{{ route('comics.show', 'comic') }}" class="btn btn-success"><i
+                                class="fa-solid fa-eye"></i></a>
+                        <a href="{{ route('comics.edit', 'comic') }}" class="btn btn-warning"><i
+                                class="fa-solid fa-pencil"></i></a>
+                    </div>
+                </td>
+            </tr>
+
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+@endsection
+
+
+@section('titlePage')
+I miei Fumetti
+@endsection
