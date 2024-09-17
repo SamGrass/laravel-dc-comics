8674cfe2d105d164e4541a5bfd78b22766ddba47
Samgrass <samuelegrassetti01@gmail.com>
2024-09-17 19:02:24 +0200

    create edit page

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container my-5">
+    <h1>Modifica fumetto</h1>
+
+</div>
+
+@endsection
+
+
+@section('titlePage')
+I miei Fumetti
+@endsection
