8fd53feb5a1e6107378777461c06faee037f21f3
Samgrass <samuelegrassetti01@gmail.com>
2024-09-18 16:00:07 +0200

    create: request validation for comics form

---
+++ app/Http/Requests/ComicRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|min:3|max:150',
+            'thumb' => 'required|active_url',
+            'price' => 'required|min:3|max:10',
+            'series' => 'required|min:3|max:50',
+            'sale_date' => 'required|date',
+            'type' => 'required|min:3|max:50',
+        ];
+    }
+}
